abe58af400d0f20562b673c1b60dd53f4a6c9167
YWQ <2493039625@qq.com>
2024-07-22 00:14:58 +0800

    git push shell

---
+++ git-pull.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env pwsh
+
+#输入变量
+
+#定义变量
+REMOTE_REPOSITORY_SITE="https://github.com/tiaxiaoer/mit6.s081-exercise.git"
+REMOTE_REPOSITORY_NAME="origin"
+BRANCH=$1
+
+git pull $REMOTE_REPOSITORY_NAME $1:$1
+
