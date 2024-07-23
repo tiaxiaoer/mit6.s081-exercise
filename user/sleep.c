312ac6308d8ee68a827798472c77c9c5f3ccf499
YWQ <2493039625@qq.com>
2024-07-23 15:55:38 +0800

    add user/sleep.c code

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "stdlib.h"
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int sleep(int n){
+    for(int i=0; i<n; i++){
+        sys_sleep();
+    }
+}
+
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        fprintf(2, "usage: sleep time long\n");
+        exit(1);
+    } 
+    sleep(atoi(argv[1]));
+    exit(0);
+}
