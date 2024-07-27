a5278a79b2194ec48f9d968abc7dab32a70a6391
YWQ <2493039625@qq.com>
2024-07-27 18:46:27 +0800

    add pingpong.c

---
+++ user/pingpong.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    int pidfd[2];
+    if(pipe(pidfd)==-1){
+        printf("pipe error\n");
+        exit(1);
+    }
+    pid_t pid;
+    pid=fork();
+    char str[100];
+    if(pid==0){ //子进程 
+        read(pidfd[0], str, sizeof(str));
+        pid_t temp_pid=get_pid();
+        fprintf(str, "<%u>: received ping", (unsigned int)temp_pid);
+        write(pidfd[1], str, strlen(str)+1);
+        close(pidfd[1]);
+    }else{      //父进程
+        close(pidfd[0]);
+        write(pidfd[1], "hello!", strlen("hello!")+1 );
+        close(pidfd[1]);
+        read(pidfd[0], str, sizeof(str));
+        printf("<%u>:: received pong", (unsigned int)get_pid());
+    }
+    exit(0);
+}
