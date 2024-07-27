9d355b23e9434e32212658e98f416219daeead47
YWQ <2493039625@qq.com>
2024-07-27 19:30:40 +0800

    change pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -15,20 +15,16 @@ int main(int argc, char *argv[]){
     char str[100];
     if(pid==0){ //子进程 
         read(pidfd[0], str, sizeof(str));
-        /*
-        pid_t temp_pid=getpid();
-        fprintf(str, "<%u>: received ping", (unsigned int)temp_pid);
-        write(pidfd[1], str, strlen(str)+1);
-        */
         fprintf((int)pidfd[1], "<%u>: received ping", (unsigned int)getpid());
-        printf("<%u>:: received ping", (unsigned int)getpid());
+        printf("<%u>:: received ping\n", (unsigned int)getpid());
         close(pidfd[1]);
     }else{      //父进程
         close(pidfd[0]);
         write(pidfd[1], "hello!", strlen("hello!")+1 );
         close(pidfd[1]);
         read(pidfd[0], str, sizeof(str));
-        printf("<%u>:: received pong", (unsigned int)getpid());
+        printf("<%u>:: received pong\n", (unsigned int)getpid());
+        close(pidfd[1]);
     }
     exit(0);
 }
